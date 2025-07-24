8dc347ebe5923bbdb65765106bdf4a60365a2217
frankydevai <96786170+frankydevai@users.noreply.github.com>
2025-07-24 23:30:35 +0500

    Update homework4.sql

--- lesson-4/homework/homework4.sql
+++ lesson-4/homework/homework4.sql
@@ -1,3 +1,5 @@
+use homework
+
 CREATE TABLE Employees (
     EmployeeID INT PRIMARY KEY,
     FirstName VARCHAR(50) NULL,
@@ -210,3 +212,51 @@ Select coalesce ( firstname, lastname) from Employees
 --- 13 Write a query that selects distinct Category and Price from the Products table.
 
 Select distinct Category, Price from Products
+
+--- 14 Write a query that filters the Employees table to show employees whose Age is either between 30 and 40 or Department = 'Marketing'.
+Select  firstname, lastname from Employees where Age between 30 and 40 or DepartmentName = 'Marketing'
+
+--- 15 Use OFFSET-FETCH to select rows 11 to 20 from the Employees table, ordered by Salary DESC 
+select  * from Employees
+  order by salary desc
+  offset 10  row fetch next 10 rows only
+
+---- 16 Write a query to display all products with Price <= 1000 and Stock > 50, sorted by Stock in ascending order
+
+Select * from Products where Price <=1000 and StockQuantity > 50 Order by  StockQuantity asc
+
+--- 17 Write a query that filters the Products table for ProductName values containing the letter 'e' using LIKE.
+
+Select * from Products where  productName Like '%e%' 
+
+--- 
+--- 18 Use IN operator to filter for employees who work in either 'HR', 'IT', or 'Finance'.
+
+Select FirstName, DepartmentName from employees where  DepartmentName in ('HR', 'IT',  'Finance')
+
+--- 19 Use ORDER BY to display a list of customers ordered by City in ascending and PostalCode in descending order.Use customers table
+
+Select * from Customers order by City asc,  Postalcode desc
+
+  --- 🔴 Hard-Level Tasks
+--- 20 Write a query that selects the top 5 products with the highest sales, using TOP(5) and ordered by SalesAmount DESC.
+Select  top 5 ProductName from Products order by StockQuantity desc
+---- 21 Combine FirstName and LastName into one column named FullName in the Employees table. (only in select statement)
+Select (Firstname + ' '+lastname)as fullname from employees 
+
+---- 22 Write a query to select the distinct Category, ProductName, and Price for products that are priced above $50, using DISTINCT on three columns
+Select distinct  category, ProductName,  Price  from Products where price > 50
+---- 23 Write a query that selects products whose Price is less than 10% of the average price in the Products table. (Do some research on how to find average price of all products)
+Select productname  from products  where  price < (avg(price)  % 10) 
+---- 24 Use WHERE clause to filter for employees whose Age is less than 30 and who work in either the 'HR' or 'IT' department.
+select firstname , lastname, age, DepartmentName from Employees where age < 30 and DepartmentName IN ('HR', 'IT')
+---- 25 Use LIKE with wildcard to select all customers whose Email contains the domain '@gmail.com'.
+
+Select *  from Customers where  Email Like '%@gmail.com%'
+
+---- 26 Write a query that uses the ALL operator to find employees whose salary is greater than all employees in the 'Sales' department.
+
+Select  *  from employees where salary > all(select salary from Employees where DepartmentName = 'Sales')
+---- 27. Write a query that filters the Orders table for orders placed in the last 180 days using BETWEEN and LATEST_DATE in the table. (Search how to get the current date and latest date)
+
+Select *, MAX(orderdate) AS LatestOrderDatefrom  from Orders where  LatestOrderDatefrom 
